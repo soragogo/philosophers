7713acf3fa63f03d8b8a4c1b395745bf73531a42
Emu Kamada <ekamada@c1r21s4.42tokyo.jp>
2023-09-25 21:50:34 +0900

    gettimeofdayを使う

--- main.c
+++ main.c
@@ -2,9 +2,18 @@
 
 void *start_routine(void *arg)
 {
-    int *p = (int *)arg;
-    usleep(30);
-    printf("%d\n", *p);
+    t_list *p = (t_list *)arg;
+    struct timeval tv;
+
+    usleep(300);
+    p->time_to_die = 123456;
+    for (int i = 0; i < 1000; i++)
+    {
+        gettimeofday(&tv, NULL);
+        usleep(20);
+        // printf("%d\n", tv.tv_usec);
+    }
+    printf("%d\n", p->time_to_die);
 }
 
 int     *create_forks(int num_of_philos)
@@ -13,7 +22,7 @@ int     *create_forks(int num_of_philos)
     int     i;
 
     i = 0;
-    forks = malloc(sizeof(int)* num_of_philos);
+    forks = calloc(num_of_philos, sizeof(int));
     if (!forks)
         return (NULL);
     while (i < num_of_philos)
@@ -21,26 +30,26 @@ int     *create_forks(int num_of_philos)
         forks[i] = 1;
         i++;
     }
-    return forks;  
+    return forks;
 }
 
 t_list *create_threads(t_list *list, int *args, int *forks)
 {
     int     i;
 
-    i = 0;  
-    list = malloc(sizeof(t_list) * args[0]);
+    i = 0;
+    list = calloc(args[0], sizeof(t_list));
     if (!list)
         return (NULL);
     while (i < args[0])
     {
         list[i].philo = i;
-        pthread_create(&(list[i].thread), NULL, start_routine, &(forks[i]));
+        pthread_create(&(list[i].thread), NULL, start_routine, &list[i]);
         list[i].my_fork[0] = &forks[i];
         if (i + 1 < args[0])
             list[i].my_fork[1] = &forks[i + 1];
         else
-            list[i].my_fork[1] = &forks[0];  // add circularity for the last philosopher's right fork
+            list[i].my_fork[1] = &forks[0];
         pthread_join(list[i].thread, NULL);
         i++;
     }
@@ -64,8 +73,8 @@ int main(int ac, char *av[])
     for (int i = 0 ; i < 5; i++)
         printf("args[%d] :%d\n", i, args[i]);
 
-    free(forks); // deallocate memory
-    free(list);  // deallocate memory
+    free(forks);
+    free(list);
     return 0;
 }
 
--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emukamada <emukamada@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ekamada <ekamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 11:01:06 by emukamada         #+#    #+#             */
-/*   Updated: 2023/08/03 11:01:11 by emukamada        ###   ########.fr       */
+/*   Updated: 2023/09/25 21:46:30 by ekamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@
 #include <libc.h>
 #include <unistd.h>
 #include <limits.h>
+#include <sys/time.h>
+
 
 typedef struct s_list
 {
 	unsigned int	philo;
 	pthread_t	thread;
+	unsigned int time_to_die;
 	int		*my_fork[2];
 
 } t_list;
