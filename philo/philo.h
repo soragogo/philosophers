c8469e5c428eee3584e87ba6c6c198a06dc2c36c
Emu Kamada <es377233@gmail.com>
2023-10-02 11:09:39 +0900

    壊した

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: emukamada <emukamada@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 11:01:06 by emukamada         #+#    #+#             */
-/*   Updated: 2023/09/30 18:21:58 by emukamada        ###   ########.fr       */
+/*   Updated: 2023/10/02 01:23:02 by emukamada        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,23 @@ typedef struct s_fork
 {
 	pthread_mutex_t lock;
 	bool is_available;
+	// int num;
+	int evenodd;
 } t_fork;
+
+typedef struct s_end
+{
+	pthread_mutex_t lock;
+	int flag;
+
+} t_end;
 
 typedef struct s_philo
 {
 	unsigned int name;
 	pthread_t thread;
 	t_fork *my_fork[2];
+	t_end *end_flag;
 	int die_duration;
 	int eat_duration;
 	int sleep_duration;
@@ -39,11 +49,10 @@ typedef struct s_philo
 	unsigned long time_to_eat;
 	unsigned long time_to_sleep;
 	unsigned long time_to_think;
-
 } t_philo;
 
 int error_handling(int ac, char *av[], int (*args)[5]);
 int ft_atoi(char *str);
-int philo_actions(t_philo *philo, bool *is_alive);
+int philo_actions(t_philo *philo);
 unsigned long get_time();
 #endif
--- philo/philo_action.c
+++ philo/philo_action.c
@@ -1,23 +1,58 @@
 #include "philo.h"
 
-void action_eat(t_philo *philo, bool *is_alive)
+void take_forks(t_philo *philo)
+{
+	unsigned long time;
+	pthread_mutex_lock(&(philo->my_fork[0]->lock));
+	while ((philo->my_fork[0]->evenodd % 2) ^ (philo->name % 2))
+		usleep(1);
+	time = get_time();
+	// printf("%lu %d has taken a fork[%d]\n", time, philo->name, philo->my_fork[0]->num);
+	printf("%lu %d has taken a fork\n", time, philo->name);
+	pthread_mutex_lock(&(philo->my_fork[1]->lock));
+	while ((philo->my_fork[1]->evenodd % 2) ^ (philo->name % 2))
+		usleep(1);
+	time = get_time();
+	// printf("%lu %d has taken a fork[%d]\n", time, philo->name, philo->my_fork[1]->num);
+	printf("%lu %d has taken a fork\n", time, philo->name);
+}
+
+void return_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(&(philo->my_fork[0]->lock));
+	pthread_mutex_unlock(&(philo->my_fork[1]->lock));
+}
+
+int action_eat(t_philo *philo)
 {
 	unsigned long time;
 
+	take_forks(philo);
 	time = get_time();
-	philo->time_to_die = time + philo->eat_duration;
+	philo->time_to_die = time + philo->die_duration;
 	philo->time_to_sleep = time + philo->eat_duration;
+	time = get_time();
 	printf("%lu %d is eating\n", time, philo->name);
 	while (1)
 	{
 		time = get_time();
+		if (time > philo->time_to_die)
+		{
+			printf("%lu %u died\n", time, philo->name);
+			return 1;
+		}
 		if (time > philo->time_to_sleep)
+		{
+			philo->my_fork[0]->evenodd ^= 1;
+			philo->my_fork[1]->evenodd ^= 1;
+			return_forks(philo);
 			break;
+		}
 	}
-	*is_alive = true;
+	return 0;
 }
 
-void action_sleep(t_philo *philo, bool *is_alive)
+int action_sleep(t_philo *philo)
 {
 	unsigned long time;
 
@@ -29,11 +64,16 @@ void action_sleep(t_philo *philo, bool *is_alive)
 		time = get_time();
 		if (time > philo->time_to_think)
 			break;
+		if (time > philo->time_to_die)
+		{
+			printf("%lu %u died\n", time, philo->name);
+			return 1;
+		}
 	}
-	*is_alive = false;
+	return 0;
 }
 
-void action_think(t_philo *philo, bool *is_alive)
+int action_think(t_philo *philo)
 {
 	unsigned long time;
 
@@ -44,39 +84,61 @@ void action_think(t_philo *philo, bool *is_alive)
 		time = get_time();
 		if (time > philo->time_to_eat)
 			break;
+		if (time > philo->time_to_die)
+		{
+			printf("%lu %u died\n", time, philo->name);
+			return 1;
+		}
 	}
-	*is_alive = false;
+	return 0;
 }
 
-// int action_sleep(int time_to_sleep, t_philo philo)
-// {
-// 	int started_sleep = currentTime
-// 		printf("%d %d is sleeping\n", currentTime, philo.name);
-// 	while (currentTime.tv_usec - started_sleep > time_to_sleep * 1000)
-// 	{
-// 		if (currentTime.tv_usec / 1000 >= philo.longevity)
-// 		{
-// 			printf("%d %d died\n", philo.name);
-// 			return (1);
-// 		}
-// 	}
-// 	return (0);
-// }
-
-int philo_actions(t_philo *philo, bool *is_alive)
+bool everyone_is_alive(t_philo *philo)
 {
+	bool result;
+
+	result = false;
+	pthread_mutex_lock(&(philo->end_flag->lock));
+	if (philo->end_flag->flag == 0)
+		result = true;
+	pthread_mutex_unlock(&(philo->end_flag->lock));
+	return (result);
+}
+
+void death_certificate(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->end_flag->lock));
+	philo->end_flag->flag = 1;
+	pthread_mutex_unlock(&(philo->end_flag->lock));
+}
+
+int philo_actions(t_philo *philo)
+{
+	unsigned long time;
+	time = get_time();
+	philo->time_to_die = time;
 	while (1)
 	{
-		action_eat(philo, is_alive);
-		if (is_alive == false)
+		if (everyone_is_alive(philo) == false)
 			break;
-		action_think(philo, is_alive);
-		if (is_alive == false)
-			break;
-		action_sleep(philo, is_alive);
-		if (is_alive == false)
-			break;
+		if (everyone_is_alive(philo) == true)
+			if (action_think(philo) == 1)
+			{
+				death_certificate(philo);
+				return 0;
+			}
+		if (everyone_is_alive(philo) == true)
+			if (action_eat(philo) == 1)
+			{
+				death_certificate(philo);
+				return 0;
+			}
+		if (everyone_is_alive(philo) == true)
+			if (action_sleep(philo) == 1)
+			{
+				death_certificate(philo);
+				return 0;
+			}
 	}
-
 	return 0;
 }
